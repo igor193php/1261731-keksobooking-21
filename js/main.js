a068527c29b729650c17b754da929ec1b274e44f
igor193php <iigor193@yandex.ru>
2020-10-19 00:44:42 +0300

    created validation step1

--- js/main.js
+++ js/main.js
@@ -14,32 +14,19 @@ const NUMBER_CAPACITY = [
 ];
 
 const NUMBER_KEY_ENTER = 13;
-
-const mapPinMainElement = document.querySelector('.map__pin--main');
-const mapOverlayElement = document.querySelector('.map__pins');
-const addFormElement = document.querySelector('.ad-form');
-const listAddFormElements = addFormElement.querySelectorAll('.ad-form-header, .ad-form__element');
-const mapFiltersElement = document.querySelector('.map__filters');
-const listMapFiltersElements = mapFiltersElement.querySelectorAll('.map__filter, .map__features');
-const mapElement = document.querySelector('.map');
-const roomNumberElement = document.querySelector('#room_number');
-const capacityElement = document.querySelector('#capacity');
-const adressElement = document.querySelector('#address');
-
-const mapPinElement = document.querySelector('.map__pins');
-
-let getRandomInteger = function (maxNumber, minNumber = 0) {
-  let result = Math.floor(Math.random() * maxNumber);
-
-  if (minNumber > 0) {
-    result = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-  }
-
-  return result;
-};
-
-const KEY_ENTER = 13;
 const KEY_ESC = 27;
+
+const MAX_LOCATION_Y = 630;
+const MIN_LOCATION_Y = 130;
+const MAX_ROOMS = 7;
+const MIN_ROOMS = 1;
+const MAX_GUESTS = 4;
+const MIN_GUESTS = 1;
+const MAX_GUESTS_OVER_THREE = 10;
+const MIX_GUESTS_OVER_THREE = 5;
+
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
 
 const TYPE_FLATS = [
   "palace",
@@ -66,14 +53,47 @@ const PHOTOS = [
   "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
 ];
 
-const MAX_LOCATION_Y = 630;
-const MIN_LOCATION_Y = 130;
-const MAX_ROOMS = 7;
-const MIN_ROOMS = 1;
-const MAX_GUESTS = 4;
-const MIN_GUESTS = 1;
-const MAX_GUESTS_OVER_THREE = 10;
-const MIX_GUESTS_OVER_THREE = 5;
+const ENGLISH_NAME_ROOM_TO_RUSSIAN_NAME_ROOM = {
+  flat: "Квартира",
+  bungalow: "Бунгало",
+  house: "Дом",
+  place: "Дворец"
+};
+
+const MIN_COSTS_FOR_TYPE_ROOMS = {
+  flat: 1000,
+  bungalow: 0,
+  house: 5000,
+  palace: 10000
+};
+
+const mapPinMainElement = document.querySelector('.map__pin--main');
+const mapOverlayElement = document.querySelector('.map__pins');
+const addFormElement = document.querySelector('.ad-form');
+const listAddFormElements = addFormElement.querySelectorAll('.ad-form-header, .ad-form__element');
+const mapFiltersElement = document.querySelector('.map__filters');
+const listMapFiltersElements = mapFiltersElement.querySelectorAll('.map__filter, .map__features');
+const mapElement = document.querySelector('.map');
+const roomNumberElement = document.querySelector('#room_number');
+const capacityElement = document.querySelector('#capacity');
+const adressElement = document.querySelector('#address');
+const titlePostElement = document.querySelector('#title');
+const typePostElement = document.querySelector('#type');
+const pricePostElement = document.querySelector('#price');
+
+const checkNumberIsNumeric = function (value) {
+  return /^\d+$/.test(value);
+}
+
+let getRandomInteger = function (maxNumber, minNumber = 0) {
+  let result = Math.floor(Math.random() * maxNumber);
+
+  if (minNumber > 0) {
+    result = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  }
+
+  return result;
+};
 
 let getPosts = function (number) {
   let clientWidthElement = document.querySelector('.map__pins');
@@ -158,6 +178,7 @@ const createDomItem = function (jsObject, template, parentTeg) {
 
 };
 
+pricePostElement.setAttribute('placeholder', MIN_COSTS_FOR_TYPE_ROOMS[typePostElement.value]);
 
 listMapFiltersElements.forEach(function (value) {
   value.setAttribute('disabled', 'disabled');
@@ -172,6 +193,7 @@ const actionDefaultForStart = function () {
   createDomItem(posts, pinTemplates, mapOverlayElement);
   addFormElement.classList.remove('ad-form--disabled');
   mapElement.classList.remove('map--faded');
+  adressElement.setAttribute('disabled', 'disabled');
 
   listMapFiltersElements.forEach(function (value) {
     value.removeAttribute('disabled');
@@ -181,6 +203,7 @@ const actionDefaultForStart = function () {
     value.removeAttribute('disabled');
   });
 
+  createDomItem(posts, pinTemplates, mapOverlayElement);
 };
 
 mapPinMainElement.addEventListener('click', function () {
@@ -193,39 +216,67 @@ mapPinMainElement.addEventListener('keydown', function (evt) {
   }
 });
 
-addFormElement.addEventListener('click', function () {
-
-
-  if (roomNumberElement.value === NUMBER_OF_ROOMS[0] && capacityElement.value !== NUMBER_CAPACITY[1]) {
-    capacityElement.setCustomValidity("Заселить можно не более 1 гостя");
-  } else if (roomNumberElement.value === NUMBER_OF_ROOMS[1]) {
-    if (capacityElement.value > NUMBER_CAPACITY[2] || capacityElement.value === NUMBER_CAPACITY[0]) {
-      capacityElement.setCustomValidity("Заселить можно 1 или 2 гостя");
+addFormElement.addEventListener('click', function (evt) {
+  if (evt.target.matches('#capacity')) {
+    if (roomNumberElement.value === NUMBER_OF_ROOMS[0] && capacityElement.value !== NUMBER_CAPACITY[1]) {
+      capacityElement.setCustomValidity("Заселить можно не более 1 гостя");
+    } else if (roomNumberElement.value === NUMBER_OF_ROOMS[1]) {
+      if (capacityElement.value > NUMBER_CAPACITY[2] || capacityElement.value === NUMBER_CAPACITY[0]) {
+        capacityElement.setCustomValidity("Заселить можно 1 или 2 гостя");
+      }
+    } else if (roomNumberElement.value === NUMBER_OF_ROOMS[2] && capacityElement.value === NUMBER_CAPACITY[0]) {
+      capacityElement.setCustomValidity("Заселить можно от 1 до 3 гостей");
+    } else if (roomNumberElement.value === NUMBER_OF_ROOMS[3] && capacityElement.value !== NUMBER_CAPACITY[0]) {
+      capacityElement.setCustomValidity("Гостей селить нельзя");
+    } else {
+      capacityElement.setCustomValidity('');
     }
-  } else if (roomNumberElement.value === NUMBER_OF_ROOMS[2] && capacityElement.value === NUMBER_CAPACITY[0]) {
-    capacityElement.setCustomValidity("Заселить можно от 1 до 3 гостей");
-  } else if (roomNumberElement.value === NUMBER_OF_ROOMS[3] && capacityElement.value !== NUMBER_CAPACITY[0]) {
-    capacityElement.setCustomValidity("Гостей селить нельзя");
-  } else {
-    capacityElement.setCustomValidity('');
+    capacityElement.reportValidity();
   }
 
-  capacityElement.reportValidity();
+  if (evt.target.matches('#title')) {
+    titlePostElement.setAttribute('required', '');
+    if (titlePostElement.value.length < MIN_TITLE_LENGTH) {
+      titlePostElement.setCustomValidity("Заголовок должен быть не меньше " + MIN_TITLE_LENGTH + " символов.");
+    } else if (titlePostElement.value.length > MAX_TITLE_LENGTH) {
+      titlePostElement.setCustomValidity("Заголовок должен быть не больше " + MAX_TITLE_LENGTH + " символов.");
+    } else {
+      titlePostElement.setCustomValidity('');
+    }
+    titlePostElement.reportValidity();
+  }
+
+  if (evt.target.matches('#price')) {
+    pricePostElement.setAttribute('required', '');
+    if (typePostElement.value === "bungalow" && pricePostElement.value < 0) {
+      pricePostElement.setCustomValidity("Цена за бунгало должна быть не меньше 0");
+    } else if (typePostElement.value === "flat" && pricePostElement.value < 1000) {
+      pricePostElement.setCustomValidity("Цена за квартиру должна быть не меньше " + 1000 + " рублей за ночь");
+    } else if (typePostElement.value === "house" && pricePostElement.value < 5000) {
+      pricePostElement.setCustomValidity("Цена за дом должна быть не меньше " + 5000 + " рублей за ночь");
+    } else if (typePostElement.value === "palace" && pricePostElement.value < 10000) {
+      pricePostElement.setCustomValidity("Цена за дом должна быть не меньше " + 10000 + " рублей за ночь");
+    } else if (!checkNumberIsNumeric(pricePostElement.value)) {
+      pricePostElement.setCustomValidity("Введите целое положительное число");
+    } else if (pricePostElement.value > 1000000) {
+      pricePostElement.setCustomValidity("Цена превышает максимальный размер в " + 1000000 + " рублей");
+    } else {
+      pricePostElement.setCustomValidity('');
+    }
+    pricePostElement.reportValidity();
+  }
+
+  if (evt.target.matches('#type')) {
+    pricePostElement.setAttribute('placeholder', MIN_COSTS_FOR_TYPE_ROOMS[evt.target.value]);
+  }
+
 });
 
 
 adressElement.value = mapPinMainElement.offsetLeft + ', ' + mapPinMainElement.offsetTop;
 
-createDomItem(posts, pinTemplates, mapOverlayElement);
-
-
 const createDomCard = function (jsObject, template, parentTeg) {
-  const EnglishNameRoomToRussianNameRoom = {
-    flat: "Квартира",
-    bungalow: "Бунгало",
-    house: "Дом",
-    place: "Дворец"
-  };
+
   const ItemsPost = {
     popupTitle: template.querySelector('.popup__title'),
     textAddress: template.querySelector('.popup__text.popup__text--address'),
@@ -245,7 +296,7 @@ const createDomCard = function (jsObject, template, parentTeg) {
   popupTitle.textContent = jsObject.offer.title;
   textAddress.textContent = jsObject.offer.address;
   textPrice.textContent = jsObject.offer.price + "р/ночь";
-  popupType.textContent = EnglishNameRoomToRussianNameRoom[jsObject.offer.type];
+  popupType.textContent = ENGLISH_NAME_ROOM_TO_RUSSIAN_NAME_ROOM[jsObject.offer.type];
   textCapacity.textContent = jsObject.offer.rooms + " комнаты для " + jsObject.offer.guests;
   textTime.textContent = "Заезд после " + jsObject.offer.checkin + ", выезд до " + jsObject.offer.checkout;
   popupFeatures.textContent = jsObject.offer.features;
@@ -273,18 +324,18 @@ const closePopupWindow = function () {
   buttonClosePopupElement.addEventListener('click', function () {
     popupCardElement.hidden = true;
   });
-  buttonClosePopupElement.addEventListener('keydown', function (evt) {
+  mapOverlayElement.addEventListener('keydown', function (evt) {
     if (evt.keyCode === KEY_ESC) {
       popupCardElement.hidden = true;
     }
   });
 };
 
-mapPinElement.addEventListener('click', function (evt) {
-  if (evt.target.matches('.map__pin')) {
-    const imgElement = evt.target.querySelector('img');
-    const pinLocationX = evt.target.offsetLeft - imgElement.clientWidth;
-    const pinLocationY = evt.target.offsetTop - imgElement.clientHeight;
+mapOverlayElement.addEventListener('click', function (evt) {
+  if (evt.target.matches('img')) {
+    const imgElement = evt.target;
+    const pinLocationX = evt.target.parentElement.offsetLeft - imgElement.clientWidth;
+    const pinLocationY = evt.target.parentElement.offsetTop - imgElement.clientHeight;
 
     posts.forEach(function (value) {
       if (value.location.y === pinLocationY && value.location.x === pinLocationX) {
@@ -296,8 +347,8 @@ mapPinElement.addEventListener('click', function (evt) {
   }
 });
 
-mapPinElement.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === KEY_ENTER) {
+mapOverlayElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === NUMBER_KEY_ENTER) {
     const imgElement = evt.target.querySelector('img');
     const pinLocationX = evt.target.offsetLeft - imgElement.clientWidth;
     const pinLocationY = evt.target.offsetTop - imgElement.clientHeight;
@@ -310,4 +361,5 @@ mapPinElement.addEventListener('keydown', function (evt) {
       }
     });
   }
+
 });

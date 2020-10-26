ed444e5cb43d48081403ba763d34749ed9917d68
igor193php <iigor193@yandex.ru>
2020-10-26 18:59:19 +0300

    step2

--- js/data.js
+++ js/data.js
@@ -1,6 +1,59 @@
 "use strict";
 
 (function () {
+ 
+  let getPosts = function (number) {
+
+    const clientWidthElement = document.querySelector('.map__pins');
+    let posts = [];
+
+    for (let i = 0; i < number; i++) {
+      let locationX = getRandomInteger(clientWidthElement.clientWidth);
+      let locationY = getRandomInteger(MAX_LOCATION_Y, MIN_LOCATION_Y);
+      let rooms = getRandomInteger(MAX_ROOMS, MIN_ROOMS);
+
+      let getGuests = function (numberRooms) {
+        let guests = getRandomInteger(MAX_GUESTS, MIN_GUESTS);
+        if (numberRooms > 3) {
+          guests = getRandomInteger(MAX_GUESTS_OVER_THREE, MIX_GUESTS_OVER_THREE);
+        }
+        return guests;
+      };
+
+      let getItems = function (numberItems, features) {
+        let result = [];
+        for (let j = 0; j <= numberItems; j++) {
+          result[j] = features[j];
+        }
+        return result;
+      };
+
+      const numberWithZerro = String(i + 1).padStart(2, '0');
+
+      posts[i] =
+        {
+          "author": {
+            "avatar": 'img/avatars/user' + numberWithZerro + '.png'
+          },
+          "offer": {
+            "title": "Заголовок предложения " + i,
+            "address": locationX + ", " + locationY,
+            "price": getRandomInteger(100000),
+            "type": TYPE_FLATS[getRandomInteger(3)],
+            "rooms": rooms,
+            "guests": getGuests(rooms),
+            "checkin": CHECK_TIMES[getRandomInteger(2)],
+            "checkout": CHECK_TIMES[getRandomInteger(2)],
+            "features": getItems(getRandomInteger(5), FEATURES),
+            "description": "О писание объявления " + i,
+            "photos": getItems(getRandomInteger(2), PHOTOS)
+          },
+          "location": {
+            "x": locationX,
+            "y": locationY
+          }
+        };
+    }
 
     return posts;
   };
--- js/load.js
+++ js/load.js
@@ -1,18 +1,53 @@
 "use strict";
 
 (function () {
-  const load = function (url, data, onError) {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+const load = function (url, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
 
+    xhr.responseType = 'json';
+    
     xhr.addEventListener('load', function () {
-      console.log(xhr.status + ' ' + xhr.statusText);
+        var error;
+        switch (xhr.status) {
+            case 200:
+                onSuccess(xhr.response);
+                break;
+
+            case 400: 
+                error = 'Неверный запрос';
+                break;
+            case 401:
+                error = 'Пользователь не авторизован';
+                break;
+            case 404:  
+                error = 'Ничего не найдено';
+                break;
+
+            default:
+                error = 'Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText;
+        }
+
+        if (error) {
+            onError(error);
+        }
     });
+    
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    
+    xhr.timeout = 10000; // 10s
+    
     xhr.open('GET', url);
     xhr.send();
-  };
+  }
 
-window.load = {
-  load: load
-};
+  window.load = {
+    load: load
+
+  }
 })();

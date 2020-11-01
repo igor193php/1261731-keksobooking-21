8475f50672f9292d42ec92422e05bc43c72a588c
igor193php <iigor193@yandex.ru>
2020-11-01 23:21:52 +0300

    step1

--- js/move.js
+++ js/move.js
@@ -1,19 +1,12 @@
 "use strict";
 
 (function () {
-  const mapPinMainElement = window.main.mapPinMainElement;
 
-  mapPinMainElement.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-
-  let startCoords = {
-x: evt.clientX,
-y: evt.clientY
-  };
-  console.log(startCoords.y);
-  const onMouseMove = function (moveEvt) {
+  const onMouseMove = function (moveEvt, startCoords, mapPinMainElement) {
     moveEvt.preventDefault();
-    console.log(startCoords.y);
+
+
+
     const shift = {
       x: startCoords.x - moveEvt.clientX,
       y: startCoords.y - moveEvt.clientY
@@ -23,12 +16,22 @@ y: evt.clientY
       x: moveEvt.clientX,
       y: moveEvt.clientY
     };
-const offsetLeftStylMapPin = mapPinMainElement.offsetLeft;
-const offsetTopStylMapPin = mapPinMainElement.offsetTop;
-mapPinMainElement.setAttribute("style", "left: " + (offsetLeftStylMapPin - shift.x) + "px, " + "top: " + (offsetTopStylMapPin - shift.y) + "px;");
+
+    const offsetLeftStylMapPin = mapPinMainElement.offsetLeft;
+
+    const offsetTopStylMapPin = mapPinMainElement.offsetTop;
+    mapPinMainElement.setAttribute("style", "left: " + (offsetLeftStylMapPin - shift.x) + "px; " + "top: " + (offsetTopStylMapPin - shift.y) + "px;");
   };
 
-document.addEventListener('mousemove', onMouseMove);
-//document.addEventListener('mouseup', onMouseUp);
-});
+  const onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  window.move = {
+    onMouseMove: onMouseMove,
+    onMouseUp: onMouseUp
+  };
 })();
